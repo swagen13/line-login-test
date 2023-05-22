b627091af4cb01686fd88387cb4a023c32e46a98
swagen13 <106917556+swagen13@users.noreply.github.com>
2023-05-22 17:59:38 +0700

    line login commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import LineLogin from '@xmartlabs/react-native-line'
 
 export default function App() {
+
+  const login = async () => {
+    try {
+      const result = await LineLogin.login()
+      console.log('result', result)
+    } catch (e) {
+      console.log('error', e)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Login" onPress={() => {
+        login()
+      }} />
       <StatusBar style="auto" />
     </View>
   );
